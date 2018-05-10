0d9c1097721290982764941bac13f3469e67a475
Matheus E. Muller <hello@memuller.com>
2018-05-10 08:17:13 -0300

    Minor syntax refactoring

--- config/application.php
+++ config/application.php
@@ -63,9 +63,9 @@ define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', env('MULTISITE') ? env('MULTISITE') : false);
 define('SUBDOMAIN_INSTALL', env('SUBDOMAIN_INSTALL'));
 define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE'));
-define('PATH_CURRENT_SITE', env('PATH_CURRENT_SITE') ? env('PATH_CURRENT_SITE') : '/');
-define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ? env('SITE_ID_CURRENT_SITE') : 1);
-define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ? env('BLOG_ID_CURRENT_SITE') : 1);
+define('PATH_CURRENT_SITE', env('PATH_CURRENT_SITE') ?: '/');
+define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);
+define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?: 1);
 
 /**
  * Authentication Unique Keys and Salts
