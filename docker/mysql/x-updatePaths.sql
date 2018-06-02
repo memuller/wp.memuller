cbd41d0ef02ee0006b27e20dc50d1d297773c9d1
Matheus E. Muller <hello@memuller.com>
2018-06-02 09:58:12 -0300

    Uses domains for jensm instead of a subdirectory

--- docker/mysql/x-updatePaths.sql
+++ docker/mysql/x-updatePaths.sql
@@ -4,8 +4,8 @@
 UPDATE xm_site SET domain = REPLACE(domain, "memuller.com", "memuller.test");
 
 UPDATE xm_blogs SET domain = REPLACE(domain, "memuller.com", "memuller.test");
-UPDATE xm_blogs SET domain = "memuller.test", path = "/jensm" WHERE domain = "jeancatanho.com";
+UPDATE xm_blogs SET domain = REPLACE(domain, "jeancatanho.com", "jeancatanho.test");
 
 UPDATE xm_options SET option_value = REPLACE(option_value, "memuller.com", "memuller.test");
 UPDATE xm_2_options SET option_value = REPLACE(option_value, "memuller.com", "memuller.test");
-UPDATE xm_3_options SET option_value = REPLACE(option_value, "jeancatanho.com", "memuller.test/jensm");
+UPDATE xm_3_options SET option_value = REPLACE(option_value, "jeancatanho.com", "jeancatanho.test");
