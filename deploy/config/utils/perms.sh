c293e8e229b7a1bde2be73ec5b8668cbabd0fe73
Matheus E. Muller <hello@memuller.com>
2018-06-03 12:52:00 -0300

    Updates cache config permission on setup

--- deploy/config/utils/perms.sh
+++ deploy/config/utils/perms.sh
@@ -1,3 +1,4 @@
 chown -R www-data.www-data /home/wp/data/uploads ;
 chown -R www-data.www-data /home/wp/data/cache ;
 chown -R www-data.www-data /home/wp/data/config/wp-cache-config.php
+chown -R www-data.www-data /home/wp/data/config/advanced-cache.php
