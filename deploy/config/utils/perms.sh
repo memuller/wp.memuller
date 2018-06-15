1679186e5dea6ff00d4938a02d1f5c66c222dcc1
Matheus E. Muller <hello@memuller.com>
2018-06-15 11:11:45 -0300

    Improves perms-fixing routine

--- deploy/config/utils/perms.sh
+++ deploy/config/utils/perms.sh
@@ -1,4 +1,4 @@
-chown -R www-data.www-data /home/wp/data/uploads ;
-chown -R www-data.www-data /home/wp/data/cache ;
-chown -R www-data.www-data /home/wp/data/config/wp-cache-config.php
-chown -R www-data.www-data /home/wp/data/config/advanced-cache.php
+chown -R wp.wp /home/wp/repos/memuller/web/app ;
+
+cd /home/wp/repos/memuller/web/app ;
+chown -R www-data.www-data uploads cache wp-cache-config.php advanced-cache.php plugins/wp-super-cache ;
