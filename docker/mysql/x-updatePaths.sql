f8982ef83a75d9a9195b37e13b61d202a12c5bc2
Matheus E. Muller <hello@memuller.com>
2018-06-16 20:40:56 -0300

    Removes HTTPS on local docker setup

--- docker/mysql/x-updatePaths.sql
+++ docker/mysql/x-updatePaths.sql
@@ -9,3 +9,7 @@ UPDATE xm_blogs SET domain = REPLACE(domain, "jeancatanho.com", "jeancatanho.tes
 UPDATE xm_options SET option_value = REPLACE(option_value, "memuller.com", "memuller.test");
 UPDATE xm_2_options SET option_value = REPLACE(option_value, "memuller.com", "memuller.test");
 UPDATE xm_3_options SET option_value = REPLACE(option_value, "jeancatanho.com", "jeancatanho.test");
+
+UPDATE xm_options SET option_value = REPLACE(option_value, "https://", "http://") WHERE option_name = 'siteurl' OR option_name = 'home';
+UPDATE xm_2_options SET option_value = REPLACE(option_value, "https://", "http://") WHERE option_name = 'siteurl' OR option_name = 'home';
+UPDATE xm_3_options SET option_value = REPLACE(option_value, "https://", "http://") WHERE option_name = 'siteurl' OR option_name = 'home';
